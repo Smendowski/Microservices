4638617206fdfecb5be3af2b4914c30f92def8d9
SmendowskiMateusz <smendowski@student.agh.edu.pl>
2021-04-05 14:53:34 +0200

    Django and MySQL db Setup in containers

---
+++ products/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.CharField(max_length=200)
+    likes = models.PositiveIntegerField(default=0)
+
+
+class User(models.Model):
+    # Only ID field
+    pass
+
+
+
