8a0dd1109a437fd98325b4c4c329e29e5e25cf6f
Smendowski <m.smendowski@gmail.com>
2021-04-06 09:58:47 +0200

    First endpoint definition and connection - ViewSet

--- products/views.py
+++ products/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from products.models import Product
+from products.serializers import ProductSerializer
 
-# Create your views here.
+
+class ProductViewSet(viewsets.ViewSet):
+    def list(self, request):  # /api/products [GET]
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):  # /api/products [POST]
+        pass
+
+    def retrieve(self, request, pk=None):  # /api/products/<string:id> [GET]
+        pass
+
+    def update(self, request, pk=None):
+        pass
+
+    def destroy(self, request, pk=None):
+        pass
